d2f9904785b10243fc127626220f4e45c029bb06
Anirban <anirbanbhandari1@gmail.com>
2023-04-19 09:33:13 +0530

    comment update
    comment update

--- src/test/java/day11/Program1.java
+++ src/test/java/day11/Program1.java
@@ -1,9 +1,23 @@
 package day11;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");// 5
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@title='Search for products, brands and more']"))
+				.sendKeys("oppo" + Keys.ENTER);
 
 	}
 
