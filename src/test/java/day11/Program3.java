cb6ab6e52926f033a8bcdc9df6292ac4b110ae97
Anirban <anirbanbhandari1@gmail.com>
2023-05-24 10:19:38 +0530

    WebDriverWait commented

--- src/test/java/day11/Program3.java
+++ src/test/java/day11/Program3.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class Program3 {
 
